48daef79e4ead2a22853add6625c50f58b45b407
LifeShouldBeAMusical <ricki.jay@wikimedia.de>
2026-04-26 01:56:07 +0200

    Replace Existing

--- server/resolver/add_recipe.py
+++ server/resolver/add_recipe.py
@@ -1,7 +1,8 @@
+from sqlalchemy import select
 from strawberry import Info
 
 from database_connection import get_async_session
-from model.database import ComponentModel, RecipeModel
+from model.database import BaseIngredientModel, ComponentModel, RecipeModel
 from model.strawberry.model import Recipe, RecipeInput
 from resolver.find_ingredient_or_subrecipe import find_ingredient_or_subrecipe
 
@@ -10,6 +11,21 @@ async def add_recipe_mutation(self, info: Info, recipe: RecipeInput) -> Recipe:
     model = RecipeModel(title=recipe.title)
 
     async with get_async_session() as async_session:
+        async_session.add(model)
+        await async_session.refresh(model)
+
+        existing_ingredient_model = (
+            await async_session.scalars(
+                select(BaseIngredientModel).where(
+                    BaseIngredientModel.title.like(recipe.title)
+                )
+            )
+        ).one_or_none()
+        if existing_ingredient_model is not None:
+            for comp in existing_ingredient_model.components:
+                comp.sub_recipe = model
+            await async_session.delete(existing_ingredient_model)
+
         for ingredient in recipe.ingredients:
             component_model = (
                 ComponentModel(
