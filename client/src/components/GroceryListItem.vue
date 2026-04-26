dfb74e70fd2fec1b0416711d995d335535614ed7
LifeShouldBeAMusical <ricki.jay@wikimedia.de>
2026-04-26 20:44:48 +0200

    Subtitle

--- client/src/components/GroceryListItem.vue
+++ client/src/components/GroceryListItem.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import type { GroceryItemFragment } from '@/gql/types';
-import RecipeAndParents from './RecipeAndParents.vue';
+import type { GroceryItemFragment } from '@/gql/types'
+import RecipeAndParents from './RecipeAndParents.vue'
 
 defineProps<{ groceryItem: GroceryItemFragment }>()
 </script>
 
 <template>
-	<v-list-item :title="groceryItem.title">
-		<v-list-item-subtitle>{{ groceryItem.id }}</v-list-item-subtitle>
+	<v-list-item :title="groceryItem.title" :subtitle="groceryItem.id">
 		<span v-if="groceryItem.recipes">
-			(
 			<template v-for="(r, i) in groceryItem.recipes" :key="r.id">
 				<template v-if="i > 0">, </template>
 				<recipe-and-parents :recipe="r" />
 			</template>
-			)
 		</span>
 	</v-list-item>
 </template>
